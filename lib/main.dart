9f27517c4e7b08c9dde72cf9dccb6e9e1f9ea449
Harley Gotardo <106173250+HarleyGotardo@users.noreply.github.com>
2024-01-28 08:22:28 +0800

    Created Separate File for Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:android_mims_development/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,82 +18,6 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Roboto',
       ),
       home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Login Page', 
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-          ),
-          textAlign: TextAlign.center,
-          ),
-        backgroundColor: Colors.blue,),
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 100.0),
-              const Text(
-                'Mobile Inventory and Sales Management System',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 48.0),
-              const SizedBox(height: 48.0),
-              const TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: 'Enter your email',
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter your password',
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Add login functionality here
-                },
-                child: const Text('LOGIN'),
-              ),
-              const SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Add forgot password functionality here
-                },
-                child: const Text('Forgot Password?'),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
